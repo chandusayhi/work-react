767daf1c6112c5ba40c8cc124d49401265276fff
Chandrakant Hatti <chandusayhi@users.noreply.github.com>
2023-07-06 15:36:29 +0530

    redux

--- src/index.js
+++ src/index.js
@@ -1,13 +1,28 @@
+
+import '@fortawesome/fontawesome-free/css/all.css';
+
+import'mdbootstrap/css/bootstrap.css';
+import'mdbootstrap/css/mdb.css';
+
+
+
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {Provider} from "react-redux";
+import {store} from "./redux/store";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
+
+
 root.render(
   <React.StrictMode>
+<Provider store={store}>
     <App />
+</Provider>
   </React.StrictMode>
 );
 
